1ce9d96f1ab501268fc7e83204afe619d54ea293
1911086 <1911086@01-19756.pusd.dom>
2022-04-18 15:07:38 -0700

    started using time millis

--- Tower_Defense/src/Frame.java
+++ Tower_Defense/src/Frame.java
@@ -26,6 +26,7 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	JFrame f = new JFrame("Tower Defense");
 	Background back = new Background(0, 0);
 	Point p = MouseInfo.getPointerInfo().getLocation();
+	ArrayList<Virus> virus = new ArrayList<Virus>(); 
 	Virus v1 = new Virus(0, 435, 5);
 	Virus temp = new Virus(0, 435, 5);
 	Virus v2 = new Virus(0, 435, 5);
@@ -33,7 +34,8 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	Virus v4 = new Virus(0, 435, 5);
 	Virus v5 = new Virus(0, 435, 5);
 	Virus v6 = new Virus(0, 435, 5);
-//	Virus temp;
+	public long start = System.currentTimeMillis();
+
 	//private boolean yesSpawn = false;
 	private int yesSpawn = 0;
 	public int difficulty; // 0 = easy, 1 = medium, 2 = hard
@@ -47,35 +49,43 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 		checkHover();
 		super.paint(g);
 		back.paint(g);
-		v1.paint(g);
 		p = MouseInfo.getPointerInfo().getLocation();
+		long timeAttack = System.currentTimeMillis() - start;
 		
-		if(!isOnHomescreen) {
-			v1.setGameStarted();
-			v1.spawn1();
+		for(Virus v: virus) {
+			v.paint(g);
+			if(!isOnHomescreen) {
+				v.setGameStarted();
+				v1.spawn1();
+				if(timeAttack >= 1000) {
+					//System.out.print("x ");
+					//start = System.currentTimeMillis();
+					
+				}
+//				if(v1.getX() == 150 && v1.getY() == 435) {
+//					spawnAttack(v2, g);
+//				}
+//				if(v2.getX() == 150 && v2.getY() == 435) {
+//					spawnAttack(v3, g);
+//				}
+//				if(v3.getX() == 150 && v3.getY() == 435) {
+//					spawnAttack(v4, g);
+//				}
+//				if(v4.getX() == 150 && v4.getY() == 435) {
+//					spawnAttack(v5, g);
+//				}
+//				if(v5.getX() == 150 && v5.getY() == 435) {
+//					spawnAttack(v6, g);
+//				}
+				
+				//reset
+				if(v.getX() > 500 && v.getY() > 550) {
+					attackReset(v);
+				}
+			}
+			
 		}
 		
-//		rNum = (int)(Math.random() * 6 + 1);
-		if(v1.getX() == 150 && v1.getY() == 435) {
-			yesSpawn++;
-//		}else if(v1.getX() > 500 && v1.getY() > 550) {
-//			yesSpawn = true;
-		}
-		if(temp.getX() == 150 && temp.getY() == 435) {
-			yesSpawn++;
-		}
-		if(yesSpawn == 1) {
-			spawnAttack(temp, g);
-		}
-		if(yesSpawn == 2) {
-			spawnAttack(v2, g);
-		}
-		
-		
-		//reset
-		if(v1.getX() > 500 && v1.getY() > 550) {
-			attackReset(v1);
-		}
 
 	}
 	public static void main(String[] arg) {
@@ -134,18 +144,21 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 					back.setBackground("/imgs/Background.png");
 					back.returnMenu();
 					isOnHomescreen = false;
+					addToArrayList(difficulty);
 				}
 				if (p.x >= 250 && p.x <= 365 && p.y >= 340 && p.y <= 385) {
 					difficulty = 1;
 					back.setBackground("/imgs/Background.png");
 					back.returnMenu();
 					isOnHomescreen = false;
+					addToArrayList(difficulty);
 				}
 				if (p.x >= 410 && p.x <= 505 && p.y >= 340 && p.y <= 385) {
 					difficulty = 2;
 					back.setBackground("/imgs/Background.png");
 					back.returnMenu();
 					isOnHomescreen = false;
+					addToArrayList(difficulty);
 				}
 			}
 			else {
@@ -153,8 +166,11 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 				if (p.x >= 20 && p.x <= 115 && p.y >= 50 && p.y <= 95) {
 					back.returnToMenu();
 					isOnHomescreen = true;
-					v1.homescreenVirus();
-					temp.homescreenVirus();
+					for(Virus v: virus) {
+						v.homescreenVirus();
+					}
+					//v1.homescreenVirus();
+					//temp.homescreenVirus();
 				}
 			}
 		}
@@ -218,7 +234,9 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	}
 	
 	public void spawnAttack(Virus v, Graphics g) {
-		v.paint(g);
+		//v.paint(g);
+		v.setX(0);
+		v.setY(435);
 		if(rNum == 1) {
 			v.spawn1();
 		}else if(rNum == 2) {
@@ -232,9 +250,27 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 		}else if(rNum == 6) {
 			v.spawn5();
 		}
-
+		
+		rNum = (int)(Math.random() * 6 + 1);
 		v.setGameStarted();
+		start = System.currentTimeMillis();
 	}
+	
+	public void addToArrayList(int num) {
+		virus.add(v1);
+		virus.add(v2);
+		virus.add(v3);
+		if(num > 0) {
+			virus.add(v4);
+		}
+		if(num > 1) {
+			virus.add(v5);
+			virus.add(v6);
+		}
+		
+	}
+	
+	
 	public void attackReset(Virus v) {
 		v.setX(0);
 		v.setY(435);
